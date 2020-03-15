06e10f1bd602b1976080e6cfb117e5533c906292
dannihjalta <62114817+dannihjalta@users.noreply.github.com>
2020-03-15 16:01:21 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-alert("Push here to view the shit!");
+<SCRIPT language="JavaScript">
+
+var password;
+
+var pass1="cool";
+
+password=prompt('Please enter your password to view this page!',' ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="http://brebru.com/extrainfo.html";
+    }
+</SCRIPT>
